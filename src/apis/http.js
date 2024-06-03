5fc2c4d05e7fa73fa28c78a95277761abac7925f
irpatrick <patricki@andrew.cmu.edu>
2024-06-03 14:35:17 +0200

    API url changed

--- src/apis/http.js
+++ src/apis/http.js
@@ -6,7 +6,8 @@ export const ip = process.env.REACT_APP_API_URL;
 
 console.log(ip)
 export class API {
-  BASE_URL = process.env.REACT_APP_API_URL
+  // BASE_URL = process.env.REACT_APP_API_URL
+  BASE_URL = "https://rri-backend.mosipcmuafrica.me/api"
   baseJsonHeaders = {
     "Content-Type": "application/json",
   };
